0d37f1009ed03e100f5df0333c618d7fb1bb5107
Miguel Angel <dammiguelangel@gmail.com>
2021-10-08 09:36:16 +0200

    ahora si

--- chapp/src/main/java/es/iesfranciscodelosrios/chapp/model/user.java
+++ chapp/src/main/java/es/iesfranciscodelosrios/chapp/model/user.java
@@ -6,7 +6,7 @@ import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlAttribute;
 import javax.xml.bind.annotation.XmlRootElement;
 /**
- * class in which we will save a user's data
+ * class in which we will save a user's data.
  * 
  * @author Abepuoh
  *
